51226295c215a7d11c4fba7a4dd72ec75c4dcbea
WilliamC Eustis <willceusits@gmail.com>
2020-10-28 22:54:13 -0500

    Added custom length

--- app.py
+++ app.py
@@ -7,18 +7,21 @@ import transformers
 app = Flask(__name__)
 summarizer = transformers.pipeline("summarization", model = "t5-small")
 
-def summarize(s): # make this an actual summarize function
-    output = summarizer(s, min_length = 1, max_length = 200) 
+def summarize(s, length_percentage): # make this an actual summarize function
+    len_of_data = len(s.split(" "))
+    output = summarizer(s, min_length = 1, max_length = int(len_of_data*length_percentage/100))
     return output[0]['summary_text']
 
 @app.route('/', methods = ["GET", "POST"]) #
 def index():
     if request.method == 'GET':
-        return render_template(r'index.html', data="")
+        return render_template(r'index.html', data="", prefill="")
     else:
         print(request, " - - Get Request detected")
         data = request.form['transcript']
-        return render_template(r'index.html', data=summarize(str(data)))
+        length= int(request.form['length'])
+        print(length)
+        return render_template(r'index.html', data=summarize(str(data), length), prefill=data)
         # return text from the webpage
 
 
