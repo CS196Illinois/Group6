ac5f10a781c318c3a9c06edff46abc0a4c2ae671
Prajeet <prajeet2@illinois.edu>
2020-11-18 18:33:50 -0600

    Added converting video to audio file

--- SpeechtoTextConvertor/testAudio.py
+++ SpeechtoTextConvertor/testAudio.py
@@ -1,28 +1,26 @@
 import speech_recognition as sr
+import moviepy.editor
 
 
+#Converts video file to audio
+video = moviepy.editor.VideoFileClip('talking.mp4');
+audio = video.audio;
+audio.write_audiofile('SOUND_talking.wav')
 
-
+#Converts audio file to text
 def main():
-    sound = "rap.wav"
+    sound = "SOUND_talking.wav"
 
     r = sr.Recognizer()
 
 
     with sr.AudioFile(sound) as source:
         r.adjust_for_ambient_noise(source)
-
-
         print("Converting Audio To Text ..... ")
-
-
         audio = r.listen(source)
-
-
 
     try:
         print("Converted Audio Is : \n" + r.recognize_google(audio))
-
 
     except Exception as e:
         print("Error {} : ".format(e) )
